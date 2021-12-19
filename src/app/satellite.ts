46d70d5e1505d2d93712a92b3148c7cd2b860ff3
sjsbee <93555209+sjsbee@users.noreply.github.com>
2021-12-19 17:24:17 -0600

    Commit
    Added

---
+++ src/app/satellite.ts
@@ -0,0 +1,27 @@
+export class Satellite {
+
+	name: string;
+	type: string;
+	launchDate: string;
+	orbitType: string;
+	operational: boolean;
+
+ constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
+		this.name = name;
+		this.type = type;
+		this.launchDate = launchDate;
+		this.orbitType = orbitType;
+		this.operational = operational;
+   }
+	
+	isSpaceDebris(): Boolean {
+		if (this.type === "Space Debris") {
+			return true
+		} else {
+			return false
+		}
+   }
+
+}
+
+// TODO 3a: fix isSpaceDebris check
